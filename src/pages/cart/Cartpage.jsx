103b32cfee6f2cce7ed9cf9e52df42ff644be637
Ahmadreza firoozi <162193432+AhmadFiroozi@users.noreply.github.com>
2026-08-26 15:01:32 +0330

    Update Cartpage.jsx

--- src/pages/cart/Cartpage.jsx
+++ src/pages/cart/Cartpage.jsx
@@ -1,7 +1,7 @@
 import Cart from "../../components/cart/Cart";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
-import "./Cartpage.css";
+import "./CartPage.css";
 
 function CartPage() {
   return (
